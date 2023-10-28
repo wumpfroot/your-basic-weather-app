60a4a3102b16a9d8d1df31be44cdf1b03cc2de34
wumpfroot <markuskojo@msn.com>
2023-10-29 00:16:10 +0100

    added additional temperature info

--- script.js
+++ script.js
@@ -18,10 +18,12 @@ todaysDate.innerHTML = `<h2>Today is ${dayOfTheWeek} ${day}/${month}/${year}</h2
 searchBtn.addEventListener("click", () => {
 	const cityResult = document.querySelector(".search-container input").value;
 	const tempButtons = document.querySelector(".temp-conversion-btns");
+	const tempInfo = document.querySelector(".additional-temp-info");
 
 	if (cityResult === "") return;
 
 	tempButtons.style.visibility = "visible";
+	tempInfo.style.visibility = "visible";
 
 	fetch(`https://api.openweathermap.org/data/2.5/weather?q=${cityResult}&units=metric&appid=a29ad671cba80509c050c4546f651a06`)
 		.then((response) => response.json())
@@ -39,16 +41,33 @@ searchBtn.addEventListener("click", () => {
 
 			const fahrenheitConversion = document.querySelector(".fahrenheit-conversion");
 			const celciusConversion = document.querySelector(".celcius-conversion");
+			const feelsLikeTemp = document.querySelector(".feels-like");
+			const minTemp = document.querySelector(".min-temp");
+			const maxTemp = document.querySelector(".max-temp");
+
+			feelsLikeTemp.innerHTML = `<h3>${Math.round(json.main.feels_like)}°C</h3>`;
+			minTemp.innerHTML = `<h3>${Math.round(json.main.temp_min)}°C</h3>`;
+			maxTemp.innerHTML = `<h3>${Math.round(json.main.temp_max)}°C</h3>`;
 
 			fahrenheitConversion.addEventListener("click", () => {
 				const temp = document.querySelector(".temp");
-				const calculation = Math.round(json.main.temp) * 1.8 + 32;
-				temp.innerHTML = `${Math.round(calculation)}°F`;
+				const tempCalculation = Math.round(json.main.temp) * 1.8 + 32;
+				const feelsTempCalculation = Math.round(json.main.feels_like) * 1.8 + 32;
+				const minTempCalculation = Math.round(json.main.temp_min) * 1.8 + 32;
+				const maxTempCalculation = Math.round(json.main.temp_max) * 1.8 + 32;
+
+				temp.innerHTML = `${Math.round(tempCalculation)}°F`;
+				feelsLikeTemp.innerHTML = `<h3>${Math.round(feelsTempCalculation)}°F<h3>`;
+				minTemp.innerHTML = `<h3>${Math.round(minTempCalculation)}°F</h3>`;
+				maxTemp.innerHTML = `<h3>${Math.round(maxTempCalculation)}°F</h3>`;
 			});
 
 			celciusConversion.addEventListener("click", () => {
 				const temp = document.querySelector(".temp");
 				temp.innerHTML = `${Math.round(json.main.temp)}°C`;
+				feelsLikeTemp.innerHTML = `<h3>${Math.round(json.main.feels_like)}°C</h3>`;
+				minTemp.innerHTML = `<h3>${Math.round(json.main.temp_min)}°C</h3>`;
+				maxTemp.innerHTML = `<h3>${Math.round(json.main.temp_max)}°C</h3>`;
 			});
 		});
 

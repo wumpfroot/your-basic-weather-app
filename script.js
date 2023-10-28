89c5fda8d7330e1c151c1be43e3b492a1efeb54e
wumpfroot <markuskojo@msn.com>
2023-10-28 22:59:34 +0100

    Temperature conversion is working

--- script.js
+++ script.js
@@ -6,9 +6,7 @@ const infoBox = document.querySelector(".weather-info");
 searchBtn.addEventListener("click", () => {
 	const cityResult = document.querySelector(".search-container input").value;
 
-	if (cityResult === "") {
-		return;
-	}
+	if (cityResult === "") return;
 
 	fetch(`https://api.openweathermap.org/data/2.5/weather?q=${cityResult}&units=metric&appid=a29ad671cba80509c050c4546f651a06`)
 		.then((response) => response.json())
@@ -24,13 +22,21 @@ searchBtn.addEventListener("click", () => {
 			location.innerHTML = json.name;
 			temp.innerHTML = `${Math.round(json.main.temp)}°C`;
 
-			const tempConversion = Math.round(json.main.temp) * 1.8 + 32;
-			console.log(`Temp in freedom units is ${tempConversion}°F`);
+			const fahrenheitConversion = document.querySelector(".fahrenheit-conversion");
+			const celciusConversion = document.querySelector(".celcius-conversion");
+
+			fahrenheitConversion.addEventListener("click", () => {
+				const temp = document.querySelector(".temp");
+				const calculation = Math.round(json.main.temp) * 1.8 + 32;
+				temp.innerHTML = `${Math.round(calculation)}°F`;
+			});
+
+			celciusConversion.addEventListener("click", () => {
+				const temp = document.querySelector(".temp");
+				temp.innerHTML = `${Math.round(json.main.temp)}°C`;
+			});
 		});
-});
 
-const input = document.querySelector(".search-container input");
-
-input.addEventListener("click", () => {
+	const input = document.querySelector(".search-container input");
 	input.value = "";
 });

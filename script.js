df619b1966dcf92a2f4143402bab5497541f90c5
wumpfroot <markuskojo@msn.com>
2023-10-28 22:04:44 +0100

    Now able to search for city temperature

--- script.js
+++ script.js
@@ -4,7 +4,13 @@ const searchBtn = document.querySelector(".search-container button");
 const infoBox = document.querySelector(".weather-info");
 
 searchBtn.addEventListener("click", () => {
-	fetch(exampleApiCall)
+	const cityResult = document.querySelector(".search-container input").value;
+
+	if (cityResult === "") {
+		return;
+	}
+
+	fetch(`https://api.openweathermap.org/data/2.5/weather?q=${cityResult}&units=metric&appid=a29ad671cba80509c050c4546f651a06`)
 		.then((response) => response.json())
 		.then((json) => {
 			if (json.cod === "404") {
@@ -12,5 +18,19 @@ searchBtn.addEventListener("click", () => {
 				return;
 			}
 			console.log(json);
+			const location = document.querySelector(".location");
+			const temp = document.querySelector(".temp");
+
+			location.innerHTML = json.name;
+			temp.innerHTML = `${Math.round(json.main.temp)}°C`;
+
+			const tempConversion = Math.round(json.main.temp) * 1.8 + 32;
+			console.log(`Temp in freedom units is ${tempConversion}°F`);
 		});
+});
+
+const input = document.querySelector(".search-container input");
+
+input.addEventListener("click", () => {
+	input.value = "";
 });

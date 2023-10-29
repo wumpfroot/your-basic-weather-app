3a87ae60879f2a60b9ec023b9b360829bb53e35d
wumpfroot <markuskojo@msn.com>
2023-10-29 03:33:46 +0000

    Added wind details to app

--- script.js
+++ script.js
@@ -12,6 +12,9 @@ const elements = {
 	feelsLikeTemp: document.querySelector(".feels-like"),
 	minTemp: document.querySelector(".min-temp"),
 	maxTemp: document.querySelector(".max-temp"),
+	windInfo: document.querySelector(".wind-info"),
+	windSpeed: document.querySelector(".wind-speed"),
+	windDirection: document.querySelector(".wind-deg"),
 };
 
 const updateDate = () => {
@@ -24,20 +27,28 @@ const updateDate = () => {
 	elements.todaysDate.innerHTML = `<h2>Today is ${dayOfTheWeek} ${day}/${month}/${year}</h2>`;
 };
 
-const updateTemperatures = (json) => {
+const updateInfo = (json) => {
 	elements.location.innerHTML = json.name;
 	elements.temp.innerHTML = `${Math.round(json.main.temp)}°C`;
-	elements.feelsLikeTemp.innerHTML = `<h3>${Math.round(json.main.feels_like)}°C</h3>`;
-	elements.minTemp.innerHTML = `<h3>${Math.round(json.main.temp_min)}°C</h3>`;
-	elements.maxTemp.innerHTML = `<h3>${Math.round(json.main.temp_max)}°C</h3>`;
+	elements.feelsLikeTemp.innerHTML = `${Math.round(json.main.feels_like)}°C`;
+	elements.minTemp.innerHTML = `${Math.round(json.main.temp_min)}°C`;
+	elements.maxTemp.innerHTML = `${Math.round(json.main.temp_max)}°C`;
+	elements.windSpeed.innerHTML = `${Math.round(json.main.temp_max)}°C`;
+	elements.windDirection.innerHTML = `${json.wind.deg}°`;
 };
 
 const convertTemperature = (json, unit) => {
 	const tempCalculation = (temp) => (unit === "F" ? Math.round(temp * 1.8 + 32) : Math.round(temp));
 	elements.temp.innerHTML = `${tempCalculation(json.main.temp)}°${unit}`;
-	elements.feelsLikeTemp.innerHTML = `<h3>${tempCalculation(json.main.feels_like)}°${unit}<h3>`;
-	elements.minTemp.innerHTML = `<h3>${tempCalculation(json.main.temp_min)}°${unit}</h3>`;
-	elements.maxTemp.innerHTML = `<h3>${tempCalculation(json.main.temp_max)}°${unit}</h3>`;
+	elements.feelsLikeTemp.innerHTML = `${tempCalculation(json.main.feels_like)}°${unit}`;
+	elements.minTemp.innerHTML = `${tempCalculation(json.main.temp_min)}°${unit}`;
+	elements.maxTemp.innerHTML = `${tempCalculation(json.main.temp_max)}°${unit}`;
+};
+
+const convertWind = (json, unit) => {
+	const windSpeedCalculation = (speed) => (unit === "I" ? speed / 1609.34 : speed);
+	elements.windSpeed.innerHTML = `${windSpeedCalculation(json.wind.speed.toFixed(1))} m/h`;
+	elements.windDirection.innerHTML = `${json.wind.deg}°`;
 };
 
 // Search event
@@ -47,6 +58,7 @@ elements.searchBtn.addEventListener("click", () => {
 
 	elements.tempButtons.style.visibility = "visible";
 	elements.tempInfo.style.visibility = "visible";
+	elements.windInfo.style.visibility = "visible";
 
 	fetch(`https://api.openweathermap.org/data/2.5/weather?q=${cityResult}&units=metric&appid=a29ad671cba80509c050c4546f651a06`)
 		.then((response) => response.json())
@@ -56,10 +68,16 @@ elements.searchBtn.addEventListener("click", () => {
 				return;
 			}
 			console.log(json);
-			updateTemperatures(json);
+			updateInfo(json);
 
-			elements.fahrenheitConversion.addEventListener("click", () => convertTemperature(json, "F"));
-			elements.celciusConversion.addEventListener("click", () => convertTemperature(json, "C"));
+			elements.fahrenheitConversion.addEventListener("click", () => {
+				convertTemperature(json, "F");
+				convertWind(json, "I");
+			});
+			elements.celciusConversion.addEventListener("click", () => {
+				convertTemperature(json, "C");
+				convertWind(json, "M");
+			});
 		});
 
 	elements.cityInput.value = "";

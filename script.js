7833d16173c1a4351822dbdbdd4e1670dd91716f
wumpfroot <markuskojo@msn.com>
2023-10-29 02:20:51 +0000

    Refactored the JS code. Code is much cleaner

--- script.js
+++ script.js
@@ -1,76 +1,68 @@
-const searchBtn = document.querySelector(".search-container button");
-const infoBox = document.querySelector(".weather-info");
+const elements = {
+	todaysDate: document.querySelector(".date"),
+	cityInput: document.querySelector(".search-container input"),
+	searchBtn: document.querySelector(".search-container button"),
+	infoBox: document.querySelector(".weather-info"),
+	location: document.querySelector(".location"),
+	temp: document.querySelector(".temp"),
+	tempButtons: document.querySelector(".temp-conversion-btns"),
+	celciusConversion: document.querySelector(".celcius-conversion"),
+	fahrenheitConversion: document.querySelector(".fahrenheit-conversion"),
+	tempInfo: document.querySelector(".additional-temp-info"),
+	feelsLikeTemp: document.querySelector(".feels-like"),
+	minTemp: document.querySelector(".min-temp"),
+	maxTemp: document.querySelector(".max-temp"),
+};
 
-//Today's date information
-const todaysDate = document.querySelector(".date");
+const updateDate = () => {
+	const date = new Date();
+	const weekday = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+	let dayOfTheWeek = weekday[date.getDay()];
+	let day = date.getDate();
+	let month = date.getMonth() + 1;
+	let year = date.getFullYear();
+	elements.todaysDate.innerHTML = `<h2>Today is ${dayOfTheWeek} ${day}/${month}/${year}</h2>`;
+};
 
-const date = new Date();
-const weekday = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+const updateTemperatures = (json) => {
+	elements.location.innerHTML = json.name;
+	elements.temp.innerHTML = `${Math.round(json.main.temp)}°C`;
+	elements.feelsLikeTemp.innerHTML = `<h3>${Math.round(json.main.feels_like)}°C</h3>`;
+	elements.minTemp.innerHTML = `<h3>${Math.round(json.main.temp_min)}°C</h3>`;
+	elements.maxTemp.innerHTML = `<h3>${Math.round(json.main.temp_max)}°C</h3>`;
+};
 
-let dayOfTheWeek = weekday[date.getDay()];
-let day = date.getDate();
-let month = date.getMonth() + 1;
-let year = date.getFullYear();
-
-todaysDate.innerHTML = `<h2>Today is ${dayOfTheWeek} ${day}/${month}/${year}</h2>`;
+const convertTemperature = (json, unit) => {
+	const tempCalculation = (temp) => (unit === "F" ? Math.round(temp * 1.8 + 32) : Math.round(temp));
+	elements.temp.innerHTML = `${tempCalculation(json.main.temp)}°${unit}`;
+	elements.feelsLikeTemp.innerHTML = `<h3>${tempCalculation(json.main.feels_like)}°${unit}<h3>`;
+	elements.minTemp.innerHTML = `<h3>${tempCalculation(json.main.temp_min)}°${unit}</h3>`;
+	elements.maxTemp.innerHTML = `<h3>${tempCalculation(json.main.temp_max)}°${unit}</h3>`;
+};
 
 // Search event
-searchBtn.addEventListener("click", () => {
-	const cityResult = document.querySelector(".search-container input").value;
-	const tempButtons = document.querySelector(".temp-conversion-btns");
-	const tempInfo = document.querySelector(".additional-temp-info");
-
+elements.searchBtn.addEventListener("click", () => {
+	const cityResult = elements.cityInput.value;
 	if (cityResult === "") return;
 
-	tempButtons.style.visibility = "visible";
-	tempInfo.style.visibility = "visible";
+	elements.tempButtons.style.visibility = "visible";
+	elements.tempInfo.style.visibility = "visible";
 
 	fetch(`https://api.openweathermap.org/data/2.5/weather?q=${cityResult}&units=metric&appid=a29ad671cba80509c050c4546f651a06`)
 		.then((response) => response.json())
 		.then((json) => {
 			if (json.cod === "404") {
-				infoBox.innerHTML = "<h1>Oh no. This city does not exist...</h1>";
+				elements.infoBox.innerHTML = "<h1>Oh no. This city does not exist...</h1>";
 				return;
 			}
 			console.log(json);
-			const location = document.querySelector(".location");
-			const temp = document.querySelector(".temp");
+			updateTemperatures(json);
 
-			location.innerHTML = json.name;
-			temp.innerHTML = `${Math.round(json.main.temp)}°C`;
-
-			const fahrenheitConversion = document.querySelector(".fahrenheit-conversion");
-			const celciusConversion = document.querySelector(".celcius-conversion");
-			const feelsLikeTemp = document.querySelector(".feels-like");
-			const minTemp = document.querySelector(".min-temp");
-			const maxTemp = document.querySelector(".max-temp");
-
-			feelsLikeTemp.innerHTML = `<h3>${Math.round(json.main.feels_like)}°C</h3>`;
-			minTemp.innerHTML = `<h3>${Math.round(json.main.temp_min)}°C</h3>`;
-			maxTemp.innerHTML = `<h3>${Math.round(json.main.temp_max)}°C</h3>`;
-
-			fahrenheitConversion.addEventListener("click", () => {
-				const temp = document.querySelector(".temp");
-				const tempCalculation = Math.round(json.main.temp) * 1.8 + 32;
-				const feelsTempCalculation = Math.round(json.main.feels_like) * 1.8 + 32;
-				const minTempCalculation = Math.round(json.main.temp_min) * 1.8 + 32;
-				const maxTempCalculation = Math.round(json.main.temp_max) * 1.8 + 32;
-
-				temp.innerHTML = `${Math.round(tempCalculation)}°F`;
-				feelsLikeTemp.innerHTML = `<h3>${Math.round(feelsTempCalculation)}°F<h3>`;
-				minTemp.innerHTML = `<h3>${Math.round(minTempCalculation)}°F</h3>`;
-				maxTemp.innerHTML = `<h3>${Math.round(maxTempCalculation)}°F</h3>`;
-			});
-
-			celciusConversion.addEventListener("click", () => {
-				const temp = document.querySelector(".temp");
-				temp.innerHTML = `${Math.round(json.main.temp)}°C`;
-				feelsLikeTemp.innerHTML = `<h3>${Math.round(json.main.feels_like)}°C</h3>`;
-				minTemp.innerHTML = `<h3>${Math.round(json.main.temp_min)}°C</h3>`;
-				maxTemp.innerHTML = `<h3>${Math.round(json.main.temp_max)}°C</h3>`;
-			});
+			elements.fahrenheitConversion.addEventListener("click", () => convertTemperature(json, "F"));
+			elements.celciusConversion.addEventListener("click", () => convertTemperature(json, "C"));
 		});
 
-	const input = document.querySelector(".search-container input");
-	input.value = "";
+	elements.cityInput.value = "";
 });
+
+updateDate();
